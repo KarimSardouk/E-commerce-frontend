28e486a6d597ea66f04d7d578a84bc1b6e59d0ce
rebeizantoine <rebeizantoine@gmail.com>
2023-12-06 10:00:18 +0200

    added allproducts and singleproduct

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import Header from './Components/header';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/header";
+import ProductView from "./Components/ProductView"; // Import ProductView component
+import AllProducts from "./Components/AllProducts";
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/products/:productId" element={<ProductView />} />
+          <Route path="/products" element={<AllProducts />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
